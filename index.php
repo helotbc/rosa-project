3db8f281ba3dc2e20a493c905b521a6627725d94
Brendan Carroll <helotbc@gmail.com>
2024-04-28 15:41:00 -0400

    hard coded root user and password

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
 
 // These are the hardcoded database variables
 $servername = "mysql";
-$username = "brendan123";
-$password = "this-is-3857-password";
+$username = "root";
+$password = "this-is-the-root-785-password";
 $dbname = "students";
 $table = "students";
 
